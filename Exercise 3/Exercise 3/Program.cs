52bcba291730234092d380dfd042778e87ca4aa3
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-06-13 17:34:48 +0600

    I love making games!

---
+++ Exercise 3/Exercise 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 24;
+            Console.WriteLine("I am "+age+" years old");
+        }
+    }
+}
---
+++ Exercise4/Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Altitude: ");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Enter second Altitude: ");
+            int secondAltitude = int.Parse(Console.ReadLine());
+            int altitudeCovered = firstAltitude - secondAltitude;
+            Console.WriteLine("Distance Covered: "+altitudeCovered);
+        }
+    }
+}

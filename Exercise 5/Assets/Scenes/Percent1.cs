e7c88f0555b41205affa60363f97d995a3cb6a1c
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-06-14 00:00:16 +0600

    I love making websites!

---
+++ Exercise 5/Assets/Scenes/Percent1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int MaxScore = 100;
+        int score = 59;
+        float percent = (float)score / MaxScore;
+        Console.WriteLine("The score in percentage: " + percent);
+    }
+
+    // Update is called once per frame
+
+}
---
+++ FloatingPointDataTypes/FloatingPointDataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FloatingPointDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 1960;
+            int totalSecondsPlayed = 10000;
+           
+
+            float pointsPerSecond = (float)score / totalSecondsPlayed;
+            Console.WriteLine("Points Per Second: " + pointsPerSecond);
+        }
+    }
+}

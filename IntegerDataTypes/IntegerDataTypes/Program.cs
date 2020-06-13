6d4a58adae5918886e3a468231bf578e546d1f55
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-06-13 14:08:23 +0600

    I love making games!

---
+++ IntegerDataTypes/IntegerDataTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntegerDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSecondsPlayed = 250;
+            const int secondsPerMinute = 60;
+            int minutesPlayed = totalSecondsPlayed / 60;
+            Console.WriteLine("Minutes Played: "+minutesPlayed);
+            int secondsPlayed = totalSecondsPlayed % secondsPerMinute;
+            Console.WriteLine("Seconds Played: " + secondsPlayed);
+
+        }
+    }
+}

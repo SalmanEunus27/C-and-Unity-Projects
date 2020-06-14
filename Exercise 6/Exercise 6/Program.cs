215e940f0ffd2988c02fcd09447fd43d984e20ef
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-06-14 06:44:28 +0600

    I love making games!

---
+++ Exercise 6/Exercise 6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the angle in degrees: ");
+            float angle = float.Parse(Console.ReadLine());
+            float cosine = (float)Math.Cos(angle * Math.PI / 180);
+            float sine = (float)Math.Sin(angle * Math.PI / 180);
+
+            Console.WriteLine("Cosine of the angle: "+cosine);
+            Console.WriteLine("sine of the angle: "+sine);
+
+        }
+    }
+}

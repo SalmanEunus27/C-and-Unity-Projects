9d71dab3f46c68bd1ddf07176f1b3fe431e3208e
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-06-15 09:13:05 +0600

    I love making games!

---
+++ CovertTemperatures/Assets/Scenes/ConvertTemperatures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertTemperatures : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int fhTemp = 0;
+        Console.WriteLine("Initial Temperature in Fareinheit: "+fhTemp);
+        int celTemp = ((fhTemp - 32) / 9) * 5;
+        Console.WriteLine("Converted Temperature in Celsius: "+celTemp);
+        int fhtempBack = (celTemp * 5) + 32;
+        Console.WriteLine("Temperature back to Farenheit again: "+fhtempBack);
+    }
+
+    // Update is called once per frame
+    
+}
---
+++ ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProgrammingAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---This is an apllication to calculate the distance between two points and the angle between two points---");
+            Console.WriteLine("Enter first x value: ");
+            float x1 = float.Parse(Console.ReadLine());
+            float y1 = float.Parse(Console.ReadLine());
+            float x2 = float.Parse(Console.ReadLine());
+            float y2 = float.Parse(Console.ReadLine());
+            float deltax = x2 - x1;
+            float deltay = y2 - y1;
+            float  c = (float) Math.Sqrt(Math.Pow(deltax, 2) + Math.Pow(deltay,2));
+            Console.WriteLine("The distance between the two points is: "+c);
+            float radians = (float) Math.Atan2(deltay,deltax);
+            double angle = (radians * (180 / Math.PI));
+            Console.WriteLine("The angle in degrees between the two points: "+angle);
+
+        }
+    }
+}

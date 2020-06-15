9abbfcca56e74fa11b372ed9cdc4219069c6dc12
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-06-15 23:15:43 +0600

    I love making games!

--- Exercise 7.1/Assets/Scenes/NewBehaviourScript.cs
+++ Exercise 7.1/Assets/Scenes/NewBehaviourScript.cs
@@ -11,9 +11,9 @@ public class NewBehaviourScript: MonoBehaviour
         int fhTemp = 0;
         Console.WriteLine("Initial Temperature in Fareinheit: " + fhTemp);
         int celTemp = ((fhTemp - 32) / 9) * 5;
-        Console.WriteLine("Converted Temperature in Celsius: " + celTemp);
+        print("Converted Temperature in Celsius: " + celTemp);
         int fhtempBack = (celTemp * 5) + 32;
-        Console.WriteLine("Temperature back to Farenheit again: " + fhtempBack);
+        print("Temperature back to Farenheit again: " + fhtempBack);
     }
 
     // Update is called once per frame
